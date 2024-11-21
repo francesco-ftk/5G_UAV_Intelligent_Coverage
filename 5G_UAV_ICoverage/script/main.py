3f277e22f3fd28589562b7cd90bf994ecf55160d
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2024-11-21 16:37:07 +0100

    ready for overfitting

--- 5G_UAV_ICoverage/script/main.py
+++ 5G_UAV_ICoverage/script/main.py
@@ -19,14 +19,13 @@ from gym_cruising.memory.replay_memory import ReplayMemory, Transition
 from gym_cruising.neural_network.MLP_policy_net import MLPPolicyNet
 from gym_cruising.neural_network.deep_Q_net import DeepQNet
 from gym_cruising.neural_network.transformer_encoder_decoder import TransformerEncoderDecoder
-from gym_cruising.enums.constraint import Constraint
 
 UAV_NUMBER = 2
 
-TRAIN = False
+TRAIN = True
 EPS_START = 0.95  # the starting value of epsilon
 EPS_END = 0.35  # the final value of epsilon
-EPS_DECAY = 60000  # controls the rate of exponential decay of epsilon, higher means a slower decay
+EPS_DECAY = 60000  # controls the rate of exponential decay of epsilon, higher means a slower decay TODO anche 200000
 BATCH_SIZE = 256  # is the number of transitions random sampled from the replay buffer
 LEARNING_RATE = 1e-4  # is the learning rate of the Adam optimizer, should decrease (1e-5)
 BETA = 0.005  # is the update rate of the target network
@@ -45,7 +44,7 @@ print("DEVICE:", device)
 
 if TRAIN:
 
-    wandb.init(project="5G_UAV_ICoverage_Curriculum_Learning")
+    wandb.init(project="5G_UAV_ICoverage_Overfitting")
 
     env = gym.make('gym_cruising:Cruising-v0', render_mode='rgb_array', track_id=3)
     env.action_space.seed(42)
@@ -58,12 +57,12 @@ if TRAIN:
     deep_Q_net_policy = DeepQNet(state_dim=EMBEDDED_DIM).to(device)
 
     # COMMENT FOR INITIAL TRAINING
-    PATH_TRANSFORMER = '../neural_network/bestTransformer.pth'
-    transformer_policy.load_state_dict(torch.load(PATH_TRANSFORMER))
-    PATH_MLP_POLICY = '../neural_network/bestMLP.pth'
-    mlp_policy.load_state_dict(torch.load(PATH_MLP_POLICY))
-    PATH_DEEP_Q = '../neural_network/bestDeepQ.pth'
-    deep_Q_net_policy.load_state_dict(torch.load(PATH_DEEP_Q))
+    # PATH_TRANSFORMER = '../neural_network/bestTransformer.pth'
+    # transformer_policy.load_state_dict(torch.load(PATH_TRANSFORMER))
+    # PATH_MLP_POLICY = '../neural_network/bestMLP.pth'
+    # mlp_policy.load_state_dict(torch.load(PATH_MLP_POLICY))
+    # PATH_DEEP_Q = '../neural_network/bestDeepQ.pth'
+    # deep_Q_net_policy.load_state_dict(torch.load(PATH_DEEP_Q))
 
     # ACTOR POLICY NET target
     transformer_target = TransformerEncoderDecoder(embed_dim=EMBEDDED_DIM).to(device)
@@ -81,7 +80,7 @@ if TRAIN:
     optimizer_mlp = optim.Adam(mlp_policy.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
     optimizer_deep_Q = optim.Adam(deep_Q_net_policy.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
 
-    replay_buffer = ReplayMemory(100000)
+    replay_buffer = ReplayMemory(3000)
 
 
     def select_actions_epsilon(state):
@@ -115,30 +114,11 @@ if TRAIN:
         return action
 
 
-    def select_actions(state):
-        global UAV_NUMBER
-        uav_info, connected_gu_positions = np.split(state, [UAV_NUMBER * 2], axis=0)
-        uav_info = uav_info.reshape(UAV_NUMBER, 4)
-        uav_info = torch.from_numpy(uav_info).float().to(device)
-        connected_gu_positions = torch.from_numpy(connected_gu_positions).float().to(device)
-        action = []
-        with torch.no_grad():
-            tokens = transformer_policy(connected_gu_positions.unsqueeze(0), uav_info.unsqueeze(0)).squeeze(0)
-        for i in range(UAV_NUMBER):
-            with torch.no_grad():
-                # return action according to MLP [vx, vy]
-                output = mlp_policy(tokens[i])
-                output = output.cpu().numpy().reshape(2)
-                output = output * MAX_SPEED_UAV
-                action.append(output)
-        return action
-
-
     def optimize_model():
         global UAV_NUMBER
         global BATCH_SIZE
 
-        if len(replay_buffer) < 5000:
+        if len(replay_buffer) < 1000:
             return
 
         transitions = replay_buffer.sample(BATCH_SIZE)
@@ -285,52 +265,16 @@ if TRAIN:
                     1 - BETA)
         deep_Q_net_target.load_state_dict(target_net_state_dict)
 
-
-    def validate():
-        global BEST_VALIDATION
-        reward_sum = 0.0
-        seeds = [42, 15267, 98765, 54321, 24680]
-        for i in seeds:
-            state, info = env.reset(seed=i)
-            steps = 1
-            while True:
-                actions = select_actions(state)
-                next_state, reward, terminated, truncated, _ = env.step(actions)
-                reward_sum += reward
-
-                if steps == 300:
-                    truncated = True
-                done = terminated or truncated
-
-                state = next_state
-                steps += 1
-
-                if done:
-                    break
-
-        wandb.log({"reward": reward_sum})
-
-        if reward_sum > BEST_VALIDATION:
-            BEST_VALIDATION = reward_sum
-            # save the best validation nets
-            torch.save(transformer_policy.state_dict(), '../neural_network/rewardTransformer.pth')
-            torch.save(mlp_policy.state_dict(), '../neural_network/rewardMLP.pth')
-            torch.save(deep_Q_net_policy.state_dict(), '../neural_network/rewardDeepQ.pth')
-
-
     if torch.cuda.is_available():
         num_episodes = 1500
     else:
         num_episodes = 100
 
-    print("START UAV COOPERATIVE COVERAGE TRAINING")
+    print("START UAV COOPERATIVE COVERAGE TRAINING OVERFITTING")
 
     for i_episode in range(0, num_episodes, 1):
         print("Episode: ", i_episode)
         options = None
-        # Good example with GU in cluster
-        if i_episode % 25 == 0:
-            options = Constraint.CONSTRAINT802.value
         state, info = env.reset(seed=int(time.perf_counter()), options=options)
         steps = 1
         while True:
@@ -342,7 +286,8 @@ if TRAIN:
             done = terminated or truncated
 
             # Store the transition in memory
-            replay_buffer.push(state, actions, next_state, reward, int(terminated))
+            if len(replay_buffer) < 2000:
+                replay_buffer.push(state, actions, next_state, reward, int(terminated))
             # Move to the next state
             state = next_state
             # Perform one step of the optimization
@@ -352,17 +297,9 @@ if TRAIN:
             if done:
                 break
 
-        if i_episode != 0 and i_episode % 100 == 0:
-            validate()
-
-    # save the nets
-    torch.save(transformer_policy.state_dict(), '../neural_network/lastTransformer.pth')
-    torch.save(mlp_policy.state_dict(), '../neural_network/lastMLP.pth')
-    torch.save(deep_Q_net_policy.state_dict(), '../neural_network/lastDeepQ.pth')
-
     wandb.finish()
     env.close()
-    print('TRAINING COMPLETE')
+    print('TRAINING OVERFITTING COMPLETE')
 
 else:
 
@@ -399,7 +336,6 @@ else:
     PATH_MLP_POLICY = './neural_network/bestMLP.pth'
     mlp_policy.load_state_dict(torch.load(PATH_MLP_POLICY))
 
-    # options = Constraint.CONSTRAINT60.value
     options = None
 
     state, info = env.reset(seed=int(time.perf_counter()), options=options)

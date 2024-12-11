0e13734e07ec13d1041ba20cabe69768960ee3ce
francesco-ftk <francesco.fantechi3@stud.unifi.it>
2024-12-11 20:09:35 +0100

    ready for training

--- 5G_UAV_ICoverage/script/main.py
+++ 5G_UAV_ICoverage/script/main.py
@@ -32,7 +32,7 @@ sigma_policy = 0.2  # Standard deviation of noise for policy actor actions on cu
 sigma = 0.2  # Standard deviation of noise for target policy actions on next states
 c = 0.5  # Clipping bound of noise
 policy_delay = 2  # delay for policy and target nets update
-start_steps = 15000
+start_steps = 20000
 
 MAX_SPEED_UAV = 55.6  # m/s - about 20 Km/h x 10 secondi
 
